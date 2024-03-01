22509d37525e984197f834939bbf71da4a961e17
Jaremy Creechley <creechley@gmail.com>
2024-02-29 18:09:04 -0700

    test urls

--- tests/unit_tests/testpkgurls.nim
+++ tests/unit_tests/testpkgurls.nim
@@ -1,20 +1,44 @@
 
 import std/unittest
+import std/json
 
 import context, satvars, sat, gitops, runners, reporters, nimbleparser, pkgurls, cloner, versions
 import compiledpatterns
 import pkgurls
 import depgraphs
 
+template setupDepsAndGraph(url: string) =
+  var
+    p {.inject.} = initPatterns()
+    u {.inject.} = createUrl(url, p)
+    c {.inject.} = AtlasContext()
+    g {.inject.} = createGraph(c, u)
+    d {.inject.} = Dependency()
+
+suite "test pkgurls":
+
+  test "basic url":
+    setupDepsAndGraph("https://github.com/elcritch/apatheia.git")
+    check $u == "https://github.com/elcritch/apatheia.git"
+    check u.projectName == "apatheia"
+
+  test "basic url no git":
+    setupDepsAndGraph("https://github.com/elcritch/apatheia")
+    check $u == "https://github.com/elcritch/apatheia"
+    check u.projectName == "apatheia"
+
+  test "basic url prefix":
+    setupDepsAndGraph("https://github.com/elcritch/nim-apatheia")
+    check $u == "https://github.com/elcritch/apatheia"
+    check u.projectName == "apatheia"
+
+
 suite "tests":
   test "basic":
-    let
-      p = initPatterns()
-      u = createUrl("https://github.com/codex-storage/apatheia.git", p)
-    var
-      c: AtlasContext
-      g = createGraph(c, u)
-    let
-      d = Dependency()
+
+    setupDepsAndGraph("https://github.com/codex-storage/apatheia.git")
+    echo "U: ", u
+    # echo "G: ", g.toJson().pretty()
+
 
 
