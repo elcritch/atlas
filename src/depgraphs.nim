20622b42845132d0cc4b7a238006ed56cf0d6583
Jaremy Creechley <creechley@gmail.com>
2024-03-12 16:44:37 +0200

    cloning debugging

--- src/depgraphs.nim
+++ src/depgraphs.nim
@@ -148,8 +148,19 @@ iterator releases(c: var AtlasContext;
         if produced == 0:
           yield (FromHead, Commit(h: "", v: Version"#head"))
 
+      except Exception as err:
+        echo "Atlas traverseDependency error: "
+        error c, pkg.projectName, "error: " & $err.msg
+      except Defect as err:
+        echo "Atlas traverseDependency Defect: "
+        error c, pkg.projectName, "error: " & $err.msg
+      except CatchableError as err:
+        echo "Atlas traverseDependency catchable error: "
+        error c, pkg.projectName, "error: " & $err.msg
+
       finally:
         # discard exec(c, GitCheckout, [cc.get()])
+        trace c, pkg.projectName, "checking out commit: " & cc.get()
         c.checkoutGitCommit(pkg.projectName, cc.get())
     of CurrentCommit:
       yield (FromHead, Commit(h: "", v: Version"#head"))
