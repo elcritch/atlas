2ff317a22980dab9b49692c40256545b189f63a5
Jaremy Creechley <creechley@gmail.com>
2025-02-27 21:44:14 -0700

    redoing traverse tests

--- tests/testtraverse.nim
+++ tests/testtraverse.nim
@@ -2,18 +2,17 @@
 
 import std / [strutils, os, osproc, tables, sequtils, strformat, unittest]
 import basic/[sattypes, context, gitops, reporters, nimbleparser, pkgurls, compiledpatterns, versions]
-import basic/depgraphtypes
-import depgraphs
-import pkgcache
+import basic/deptypes
+import dependencies
 import testerutils
 
 if not dirExists("tests/ws_testtraverse/buildGraph"):
   ensureGitHttpServer()
 
-proc createGraph*(s: PkgUrl): DepGraph =
-  result = DepGraph(nodes: @[], reqs: defaultReqs())
-  result.packageToDependency[s] = result.nodes.len
-  result.nodes.add Dependency(pkg: s, versions: @[], isRoot: true, isTopLevel: true, activeVersion: -1)
+# proc createGraph*(s: PkgUrl): DepGraph =
+#   result = DepGraph(nodes: @[], reqs: defaultReqs())
+#   result.packageToDependency[s] = result.nodes.len
+#   result.nodes.add Dependency(pkg: s, versions: @[], isRoot: true, isTopLevel: true, activeVersion: -1)
 
 proc setupGraph*(): seq[string] =
   let projs = @["proj_a", "proj_b", "proj_c", "proj_d"]
@@ -37,9 +36,10 @@ proc setupGraphNoGitTags*(): seq[string] =
 
 suite "basic repo tests":
   setup:
-    setAtlasVerbosity(Info)
+    setAtlasVerbosity(Trace)
 
   test "ws_testtraverse collect nimbles":
+    when false:
       withDir "tests/ws_testtraverse":
         context().flags = {UsesOverrides, KeepWorkspace, ListVersions, FullClones}
         context().defaultAlgo = SemVer
@@ -50,9 +50,11 @@ suite "basic repo tests":
         let deps = setupGraph()
         var nc = NimbleContext()
         # var graph = DepGraph(nodes: @[], reqs: defaultReqs())
-        var graph = createGraph(createUrlSkipPatterns(ospaths2.getCurrentDir()))
-        graph[0].ondisk = paths.getCurrentDir()
-        graph[0].state = Found
+        let pkg = nc.createUrl(dir, projectName = pkgname)
+        let res = expand(nc, AllReleases, pkg)
+        # var graph = createGraph(createUrlSkipPatterns(ospaths2.getCurrentDir()))
+        # graph[0].ondisk = paths.getCurrentDir()
+        # graph[0].state = Found
 
         dumpJson graph, "graph-ws_testtraverse-collectnimbles.json"
 
@@ -90,6 +92,7 @@ suite "basic repo tests":
         # dumpJson graph
 
   test "ws_testtraverse releases":
+    when false:
       setAtlasVerbosity(Debug)
       withDir "tests/ws_testtraverse":
         context().flags = {UsesOverrides, KeepWorkspace, ListVersions, FullClones}
@@ -112,61 +115,68 @@ suite "basic repo tests":
   test "ws_testtraverse traverseDependency":
       # setAtlasVerbosity(Debug)
       withDir "tests/ws_testtraverse":
+        echo "RUNNING"
         context().workspace = paths.getCurrentDir()
-        context().origDepsDir = paths.getCurrentDir() / Path"buildGraph"
+        context().depsDir = paths.getCurrentDir() / Path"buildGraph"
         context().flags = {UsesOverrides, KeepWorkspace, ListVersions, FullClones}
         context().defaultAlgo = SemVer
         discard context().overrides.addPattern("$+", "file://buildGraph/$#")
 
         let deps = setupGraph()
         var nc = NimbleContext()
-        var graph = createGraph(createUrlSkipPatterns(ospaths2.getCurrentDir() ))
-        graph[0].ondisk = paths.getCurrentDir()
-        graph[0].state = Found
+        let dir = ospaths2.getCurrentDir()
+        writeFile("ws_testtraverse.nimble", "requires \"proj_a\"\n")
 
-        dumpJson graph, "graph-ws_testtraverse-traverseDependency.json"
+        let pkg = nc.createUrl(dir, projectName = "ws_testtraverse")
 
-        var i = 0
-        while i < graph.nodes.len:
-          for dep in graph.nodes.mitems():
-            if dep.state == NotInitialized:
-              let (dest, _) = pkgUrlToDirname(graph, dep)
-              dep.ondisk = dest
-              dep.state = Found
+        let specs: DependencySpecs = expand(nc, AllReleases, pkg)
 
-          traverseDependency(nc, graph, i, TraversalMode.AllReleases)
-          inc i
+        # graph[0].ondisk = paths.getCurrentDir()
+        # graph[0].state = Found
 
-        dumpJson graph, "graph-ws_testtraverse-traverseDependency-post.json"
+        # dumpJson graph, "graph-ws_testtraverse-traverseDependency.json"
 
-        check graph[0].versions.len() == 1
-        check graph.nodes.mapIt(it.pkg.projectName) == @["ws_testtraverse", "proj_a", "proj_b", "proj_c", "proj_d", "does_not_exist"]
+        # var i = 0
+        # while i < graph.nodes.len:
+        #   for dep in graph.nodes.mitems():
+        #     if dep.state == NotInitialized:
+        #       let (dest, _) = pkgUrlToDirname(graph, dep)
+        #       dep.ondisk = dest
+        #       dep.state = Found
 
-        check graph[0].pkg.projectName == "ws_testtraverse"
-        check graph[0].ondisk.string.endsWith("ws_testtraverse")
-        check graph[1].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_a")
-        check graph[2].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_b")
-        check graph[3].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_c")
-        check graph[4].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_d")
+        #   traverseDependency(nc, graph, i, TraversalMode.AllReleases)
+        #   inc i
 
-        check graph[1].versions.mapIt(($it.version, it.commit)) == @[
-            ("1.1.0", "fb3804df03c3c414d98d1f57deeb44c8a223ba44"),
-            ("1.0.0", "e479b438015e734bea67a9c63d783e78cab5746e"),
-        ]
-        check graph[2].versions.mapIt(($it.version, it.commit)) == @[
-            ("1.1.0", "ee875baecee161ed053b87b583b2f08526838bd6"),
-            ("1.0.0", "af4275109d60caaeacf2912a37c2339aca40a922"),
-        ]
-        check graph[3].versions.mapIt(($it.version, it.commit)) == @[
-            ("1.2.0", "9331e14f3fa20ed75b7d5c0ab93aa5fb0293192f"),
-            # ("1.0.0", "c7540297c01dc57a98cb1fce7660ab6f2a0cee5f"), # not tagged
-        ]
-        check graph[4].versions.mapIt(($it.version, it.commit)) == @[
-            ("2.0.0", "dd98f775ae33d450dc7f936f850e247e820e31ad"),
-            ("1.0.0", "0dec9c9733129919972416f04e73b1fb2cbf3bd3"),
-        ]
+        # dumpJson graph, "graph-ws_testtraverse-traverseDependency-post.json"
 
-        dumpJson graph, "graph-ws_testtraverse-traverseDependency-post.json"
+        # check graph[0].versions.len() == 1
+        # check graph.nodes.mapIt(it.pkg.projectName) == @["ws_testtraverse", "proj_a", "proj_b", "proj_c", "proj_d", "does_not_exist"]
+
+        # check graph[0].pkg.projectName == "ws_testtraverse"
+        # check graph[0].ondisk.string.endsWith("ws_testtraverse")
+        # check graph[1].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_a")
+        # check graph[2].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_b")
+        # check graph[3].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_c")
+        # check graph[4].ondisk.string.endsWith("ws_testtraverse/buildGraph/proj_d")
+
+        # check graph[1].versions.mapIt(($it.version, it.commit)) == @[
+        #     ("1.1.0", "fb3804df03c3c414d98d1f57deeb44c8a223ba44"),
+        #     ("1.0.0", "e479b438015e734bea67a9c63d783e78cab5746e"),
+        # ]
+        # check graph[2].versions.mapIt(($it.version, it.commit)) == @[
+        #     ("1.1.0", "ee875baecee161ed053b87b583b2f08526838bd6"),
+        #     ("1.0.0", "af4275109d60caaeacf2912a37c2339aca40a922"),
+        # ]
+        # check graph[3].versions.mapIt(($it.version, it.commit)) == @[
+        #     ("1.2.0", "9331e14f3fa20ed75b7d5c0ab93aa5fb0293192f"),
+        #     # ("1.0.0", "c7540297c01dc57a98cb1fce7660ab6f2a0cee5f"), # not tagged
+        # ]
+        # check graph[4].versions.mapIt(($it.version, it.commit)) == @[
+        #     ("2.0.0", "dd98f775ae33d450dc7f936f850e247e820e31ad"),
+        #     ("1.0.0", "0dec9c9733129919972416f04e73b1fb2cbf3bd3"),
+        # ]
+
+        # dumpJson graph, "graph-ws_testtraverse-traverseDependency-post.json"
 
   test "ws_testtraverse collectNimble no git tags":
     when false:
--- tests/ws_testtraverse/ws_testtraverse.nimble
+++ tests/ws_testtraverse/ws_testtraverse.nimble
@@ -1,2 +1,1 @@
 requires "proj_a"
-
